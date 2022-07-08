81cdff58cf68f57bc830c959f81feb4f076b9486
Ali Tonbul <alitonbul2@gmail.com>
2022-07-08 15:18:28 +0100

    fix: include lib

---
+++ lib/env.d.ts
@@ -0,0 +1,10 @@
+declare const _default: ((key: string, defaultValue?: string | undefined) => string | undefined) & {
+    int(key: string, defaultValue?: number): number | undefined;
+    float(key: string, defaultValue?: number): number | undefined;
+    bool(key: string, defaultValue?: boolean): boolean | undefined;
+    json<T>(key: string, defaultValue?: T | undefined): any;
+    array<T_1>(key: string, defaultValue?: T_1 | undefined): T_1 | string[] | undefined;
+    date(key: string, defaultValue?: Date): Date | undefined;
+};
+export default _default;
+//# sourceMappingURL=env.d.ts.map
